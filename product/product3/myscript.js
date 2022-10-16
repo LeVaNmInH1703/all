99aea2412d99307e959805a8e45ed608ad014e68
LeVaNmInH1703 <105649952+LeVaNmInH1703@users.noreply.github.com>
2022-10-16 17:02:08 +0700

    a

---
+++ product/product3/myscript.js
@@ -0,0 +1,368 @@
+const Ant = {
+    'vermelho': {
+         'id': 1,
+         'src': 'https://i.ibb.co/2nH2cTF/tenisvermelho.png',
+         'titulo': 'Nike Free Fire',
+         'tamanho': {
+            't28': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.21,
+                'box': '43 x 54 x 13'
+            },
+            't29': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.22,
+                'box': '44 x 55 x 14'
+            },
+            't30': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque': 24,
+                'peso': 0.23,
+                'box': '45 x 56 x 15'
+            },
+            't31': {
+                'preco': 168,
+                'parcelado': 43.90,
+                'estoque': 2,
+                'peso': 0.24,
+                'box': '46 x 57 x 16'
+            },
+            '32': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 21,
+                'peso': 0.25,
+                'box': '47 x 58 x 17'
+            },
+            't33': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.26,
+                'box': '48 x 59 x 18'
+            },
+            't34': {
+                'preco': 156,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.27,
+                'box': '49 x 60 x 19'
+            },
+            't35': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque':17,
+                'peso': 0.26,
+                'box': '50 x 61 x 20'
+            }
+        }
+    },
+    'azul': {
+         'id': 2,
+         'src': 'https://i.ibb.co/F0GvrRz/tenisazul.png',
+         'titulo': 'Nike Free Fire',
+         'tamanho': {
+            't28': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.21,
+                'box': '43 x 54 x 13'
+            },
+            't29': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.22,
+                'box': '44 x 55 x 14'
+            },
+            't30': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque': 24,
+                'peso': 0.23,
+                'box': '45 x 56 x 15'
+            },
+            't31': {
+                'preco': 168,
+                'parcelado': 43.90,
+                'estoque': 2,
+                'peso': 0.24,
+                'box': '46 x 57 x 16'
+            },
+            '32': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 21,
+                'peso': 0.25,
+                'box': '47 x 58 x 17'
+            },
+            't33': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.26,
+                'box': '48 x 59 x 18'
+            },
+            't34': {
+                'preco': 156,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.27,
+                'box': '49 x 60 x 19'
+            },
+            't35': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque':17,
+                'peso': 0.26,
+                'box': '50 x 61 x 20'
+            }
+        }
+    }
+    ,
+    'verde': {
+         'id': 3,
+         'src': 'https://i.ibb.co/ZH1wH3T/tenisverde.png',
+         'titulo': 'Nike Free Fire',
+         'tamanho': {
+            't28': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.21,
+                'box': '43 x 54 x 13'
+            },
+            't29': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.22,
+                'box': '44 x 55 x 14'
+            },
+            't30': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque': 24,
+                'peso': 0.23,
+                'box': '45 x 56 x 15'
+            },
+            't31': {
+                'preco': 168,
+                'parcelado': 43.90,
+                'estoque': 2,
+                'peso': 0.24,
+                'box': '46 x 57 x 16'
+            },
+            '32': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 21,
+                'peso': 0.25,
+                'box': '47 x 58 x 17'
+            },
+            't33': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.26,
+                'box': '48 x 59 x 18'
+            },
+            't34': {
+                'preco': 156,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.27,
+                'box': '49 x 60 x 19'
+            },
+            't35': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque':17,
+                'peso': 0.26,
+                'box': '50 x 61 x 20'
+            }
+        }
+    },
+    'degrade': {
+         'id': 4,
+         'src': 'https://i.ibb.co/pfzPNrs/tenisdegrade.png',
+         'titulo': 'Nike Free Fire',
+         'tamanho': {
+            't28': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.21,
+                'box': '43 x 54 x 13'
+            },
+            't29': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.22,
+                'box': '44 x 55 x 14'
+            },
+            't30': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque': 24,
+                'peso': 0.23,
+                'box': '45 x 56 x 15'
+            },
+            't31': {
+                'preco': 168,
+                'parcelado': 43.90,
+                'estoque': 2,
+                'peso': 0.24,
+                'box': '46 x 57 x 16'
+            },
+            't32': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 21,
+                'peso': 0.25,
+                'box': '47 x 58 x 17'
+            },
+            't33': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.26,
+                'box': '48 x 59 x 18'
+            },
+            't34': {
+                'preco': 156,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.27,
+                'box': '49 x 60 x 19'
+            },
+            't35': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque':17,
+                'peso': 0.26,
+                'box': '50 x 61 x 20'
+            }
+        }
+    },
+    'preto': {
+         'id': 5,
+         'src': 'https://i.ibb.co/mGMdZKs/tenispreto.png',
+         'titulo': 'Nike Free Fire',
+         'tamanho': {
+            't28': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.21,
+                'box': '43 x 54 x 13'
+            },
+            't29': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.22,
+                'box': '44 x 55 x 14'
+            },
+            't30': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque': 24,
+                'peso': 0.23,
+                'box': '45 x 56 x 15'
+            },
+            't31': {
+                'preco': 168,
+                'parcelado': 43.90,
+                'estoque': 2,
+                'peso': 0.24,
+                'box': '46 x 57 x 16'
+            },
+            't32': {
+                'preco': 150,
+                'parcelado': 31.90,
+                'estoque': 21,
+                'peso': 0.25,
+                'box': '47 x 58 x 17'
+            },
+            't33': {
+                'preco': 165,
+                'parcelado': 42.90,
+                'estoque': 1,
+                'peso': 0.26,
+                'box': '48 x 59 x 18'
+            },
+            't34': {
+                'preco': 156,
+                'parcelado': 31.90,
+                'estoque': 14,
+                'peso': 0.27,
+                'box': '49 x 60 x 19'
+            },
+            't35': {
+                'preco': 157,
+                'parcelado': 33.90,
+                'estoque':17,
+                'peso': 0.26,
+                'box': '50 x 61 x 20'
+            }
+        }
+    }
+}
+
+let tamanho = Ant.vermelho.tamanho
+let tamanhofinal
+
+function change(obj) {
+    document.getElementById('productDois').src = document.getElementById('productUm').src
+    document.getElementById('productDois').style.display = "inline" 
+    document.getElementById('productUm').src = obj.src 
+    document.getElementById('fx').className = "fx"
+    tamanho = obj.tamanho
+
+    setTimeout(() => { 
+        document.getElementById('fx').className = " "
+        document.getElementById('productDois').style.display = "none"
+    }, 1000)
+}
+
+function fillAtts(obj) {
+    document.getElementById('hold-1').innerHTML = obj.preco
+    document.getElementById('parcela').innerHTML = obj.parcelado
+    document.getElementById('peso').innerHTML = obj.peso
+    document.getElementById('box').innerHTML = obj.box
+}
+
+function acontecimentos(value) {
+    if(value === 't28') {
+        fillAtts(tamanho.t28)
+    }
+    if(value === 't29') {
+        fillAtts(tamanho.t29)
+    }
+    if(value === 't30') {
+        fillAtts(tamanho.t30)
+    }
+    if(value === 't31') {
+        fillAtts(tamanho.t31)
+    }
+    if(value === 't32') {
+        fillAtts(tamanho.t32)
+    }
+    if(value === 't33') {
+        fillAtts(tamanho.t33)
+    }
+    if(value === 't34') {
+        fillAtts(tamanho.t34)
+    }
+    if(value === 't35') {
+        fillAtts(tamanho.t35)
+    }    
+}
--- vd/main.js
+++ vd/main.js
@@ -1,107 +1,67 @@
-//khai báo các biến có bán kính, bool tự quay, tốc dộ quay,khung ảnh
-var bk = 240;
-var tu_quay = true;
-var toc_do_quay = 60;//60 s quay hết 1 vòng
-var rong = 120;
-var cao = 170;
-
-//select đến các id chọn tất cả img cho vào 1 mảng
+var imgHeight = 170;
+var imgWidth = 120;
+var autoSpin = true;
+var spinSpeed = 12;//1 vòng hết 6 s
+var r = 240;
 var ctn = document.getElementById("ctn");
-var quay_ctn = document.getElementById("quay_ctn");
-var img = document.getElementsByTagName("img");
-//gán chiều rộng và dài cho ảnh
-quay_ctn.style.width = rong +"px";
-quay_ctn.style.height = cao + "px";
+var ctnSpin = document.getElementById("ctn__spin");
+ctnSpin.style.width = imgWidth + 'px';
+ctnSpin.style.height = imgHeight + 'px';
+var imgTag = document.getElementsByTagName("img");
+var videoTag = document.getElementsByTagName("video");
+var elem = [...imgTag,...videoTag];
+//chú ý là setTimeout khi gọi hàm k có ()
+setTimeout(changeR, 2000);
+if (autoSpin) {
+    ctnSpin.style.animation = `spin ${spinSpeed}s linear infinite`;
+};
 
-setTimeout(init, 1000);
-
-//viết hàm cho nó quây thành hình tròn và gọi
-// rotateY quay theo i còn khoảng cách translateZ là giống nhau
-// cài thời gian thực hiện cho nó
-//cài thời gian delay cho cái sau phụ thuộc vào i
-//chú ý kết thúc k có ;
-function init() {
-    for (var i = 0; i < img.length; i++){
-        img[i].style.transform = "rotateY(" + (i * 360 / img.length) + "deg) translateZ(" + bk + "px)";
-        img[i].style.transition = "transform 5s";
-        // img[i].style.transitionDelay = ((img.length-i)) + "s";
-    };
+function changeR() {
+    for (var i = 0; i < elem.length; i++){
+        elem[i].style.transform = "rotateY(" + (i * 360 / elem.length) + "deg) translateZ(" + r + "px) ";
+        elem[i].style.transition = "transform 5s";
+        elem[i].style.transitionDelay = ((elem.length-i)/2) + 's';
+    }
+};
+var fx=0, fy=0, sx=0, sy=0, tx=0, ty=0, dx=0, dy=0;
+function stopSpin(autoSpin) {
+    ctnSpin.style.animationPlayState = autoSpin ? "running" : "paused";
+}
+function changeCtn() {
+    ctn.style.transform = "rotateX(" + (-ty) + "deg) rotateY(" + tx + "deg)";
 }
 
-
-//viết câu lệnh auto quay
-
-if(tu_quay) {
-    quay_ctn.style.animation = `quay ${toc_do_quay}s linear infinite`;
-}
-
-//viết hàm có dừng hay không khi click
-function playSpin(yes) {
-    quay_ctn.style.animationPlayState = (yes ? "running" : "paused");
-}
-
-var fx, fy, nx, ny, desx = 0,
-    desy = 0,
-    tx = 45,
-    ty = 45;
-
-// sx sy là tọa độ ban đầu của chuột
-//nx ny là lúc sau
-//des xy là độ biến thiên
-//tx ty là kết quả được cập nhập cho transform
-
-//viết hàm thay đổi transform
-function theo_chuot() {
-    ctn.style.transform = "rotateX(" + (-ty) + "deg) rotateY(" + (tx) + "deg)";
-}
-
-//viết hàm khi bấm chuột xuống = dừng tự quay + dừng quay quán tính + lấy tọa độ x,y
+document.onmousewheel = function (e) {
+    r += e.wheelDelta;
+    changeR();
+};
 document.onpointerdown = function (e) {
-    clearInterval(ctn.timer);
-    playSpin(false);
-    fx = e.clientX,
+    clearInterval(changeCtn.timer);
+    stopSpin(false);
+    fx = e.clientX;
     fy = e.clientY;
-    //lấy x và y theo khu vực nội tuyến
+    console.log(fx,fy);
     this.onpointermove = function (e) {
-        nx = e.clientX,
-        ny = e.clientY;
-        desx = nx - fx;
-        desy = ny - fy;
-        tx += desx * 0.1;
-        ty += desy * 0.1;
-        theo_chuot();
-        fx = nx;
-        fy = ny;
-        //khi chuột chuyển động = lấy tọa độ x,y ,tính toàn thay đổi +
-        // gọi hàm thay đổi transform cập nhập giá trị cho x, y cũ
+        sx = e.clientX;
+        sy = e.clientY;
+        dx = sx - fx;
+        dy = sy - fy;
+        tx += dx * 0.1;
+        ty += dy * 0.1;
+        changeCtn();
+        fx = sx;
+        fy = sy;
     };
     this.onpointerup = function (e) {
-        //khi nhậc chuột lên = bật tự quay + hàm quán tính
-        playSpin(true);
-        ctn.timer = setInterval(function () {
-            // hàm quán tính = setinterval + giảm dần tốc độ + cập nhập thay đổi + gọi hàm thay đổi
-            desx *= 0.99;
-            desy *= 0.99;
-            tx += desx * 0.1;
-            ty += desy * 0.1; 
-            theo_chuot();
+        stopSpin(true);
+        changeCtn.timer = setInterval(function () {
+            dx *= 0.99;
+            dy *= 0.99;
+            tx += dx*0.1;
+            ty += dy*0.1;
+            changeCtn();
         }, 17);
         this.onpointermove = this.onpointerup = null;
-    };
-    //xóa bắt sự kiện
-    return false;
-};
-//khi lăn chuột = cập nhập cho bán kính + gọi hàm thay đổi
-document.onmousewheel = function (e) {
-    bk += e.wheelDelta;
-    init();
-};
-
-
-
-
-
-
-
-
-
+    }
+    return 0;
+}
