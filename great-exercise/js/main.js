e02b3ee997232432e27db78257a20519885f9ab0
LeVaNmInH1703 <105649952+LeVaNmInH1703@users.noreply.github.com>
2022-10-20 16:55:14 +0700

    z

--- great-exercise/js/main.js
+++ great-exercise/js/main.js
@@ -2,26 +2,34 @@ var loginPage = document.getElementById('login');
 var signUpPage = document.getElementById('sign-up');
 window.onclick = function (event) {
     if (event.target == signUpPage) {
-        signUpPage.style.display = "none";
+        Close('sign-up')
     }
     if (event.target == loginPage) {
-        loginPage.style.display = "none";
+        Close("login")
     }
+}
+//
+function Close(name) {
+    var temp = document.querySelectorAll(`.${name}__account input`);
+    for (var i = 0; i < temp.length; i++){
+        temp[i].value = "";
+        label[i].classList.remove('focus');
+    }
+    var closePage = document.getElementById(`${name}`);
+    closePage.style.display = "none";
 }
 
 //
 var label = document.querySelectorAll(".sign-up__account label,.login__account label");
 var input = document.querySelectorAll(".sign-up__account input,.login__account input");
 for (let i = 0; i < label.length; i++){
-    input[i].addEventListener("checked", function () {
-        label[i].classList.add("focus")
-    })
-    input[i].addEventListener("mouseover", function () {
-        label[i].classList.add("focus")
-    })
-    input[i].addEventListener("mouseout", function () {
-        if(input[i].value=="")
-            label[i].classList.remove("focus");
-    })
+    input[i].onblur = function () {
+        if (input[i].value == '') {
+            label[i].classList.remove('focus');
+        }     
+    }
+    input[i].onfocus = function () {
+        label[i].classList.add('focus');
+    }
 }
         
--- vd/main.js
+++ vd/main.js
@@ -1,7 +1,8 @@
-import { readFile } from "fs";
-readFile("./test.txt", "utf-8", (err, data) => {
-    if (err) {
-        return console.log("err");
-    }
-    console.log(data);
-})
+var input = document.querySelector('input');
+var label = document.querySelector("label");
+
+
+ function Close () {
+    console.log("blur");
+    input.value = "";
+}
